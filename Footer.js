6688f1fc4d766d847c09566c9b5bd5c6c7a877d8
Navyasrigarika <144647488+Navyasrigarika@users.noreply.github.com>
2023-10-25 09:51:33 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Navyasrigarika'
+      className='link footer__link'
+    >
+      Created By Navya sri
+    </a>
+  </footer>
+)
+
+export default Footer
